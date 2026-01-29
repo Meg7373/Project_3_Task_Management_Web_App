67a39d32957e0a91718c9bd81610d10513e93f48
Meg7373 <meaghannlw@gmail.com>
2026-01-29 14:02:07 -0500

    delete navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import TodoList from './Components/TodoList';
-import NavBar from './Components/NavBar';
 import ContactForm from './Components/ContactForm'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
@@ -10,12 +9,10 @@ function App() {
     <BrowserRouter>
       {/* Navigation */}
       <nav className='router'>
-        <Link to="/NavBar">NavBar</Link> |{" "}
         <Link to="/TodoList">TodoList</Link> |{" "}
         <Link to="/ContactForm">ContactForm</Link>
       </nav>
       <Routes>
-        <Route path="/navbar" element={<NavBar />} />
         <Route path="/todolist" element={<TodoList />} />
         <Route path="/contactform" element={<ContactForm />} />
       </Routes>
